7d9bc56c9c377841e63116128eca981379ca8810
Kulprawee Prayoonsuk <kt.prayoonsuK@gmail.com>
2021-02-23 12:43:17 -0700

    Add files via upload

---
+++ Robot Arm/HW2_Readme.txt
@@ -0,0 +1,61 @@
+*******************************************************
+Robot Arm
+*******************************************************
+
+
+                 Read Me
+
+
+*******************************************************
+*  Description of the program:
+*******************************************************
+
+This program will create a robot arm consisting of shoulder, elbow
+arm and 3 separate single joint finger, which can be individually move by 
+corresponding keyboard keys.
+
+The program mainly utilized the glTranslate(), glRoate(), glPushMatrix(),
+glPopMatrix() and glScaled. To examine how matrix manipulation work along 
+with the matrix stack. 
+
+
+Keyboard command:
+(Clockwise / Counterclockwise)
+s/S - shoulder
+e/E - elbow
+h/H - hand(wrist)
+1/! - finger 1
+2/@ - finger 2
+3/# - finger 3
+
+*******************************************************
+*  Source files
+*******************************************************
+
+Name:	main.cpp
+	
+	The file contain all the necessary code needed to run the program.
+
+*******************************************************
+*  Circumstances of programs
+*******************************************************
+
+It was also developed, tested, compile and run 
+on Microsoft Visual Studio Community 2019
+
+STATUS: Everything work as expected. Each joint rotate correctly in the 
+	intended pivot point. 
+	
+	Please note that there were no visual
+	specification given on what the arm should look like so... im 
+	sorry if it ugly :< but it work I promise.
+
+
+*******************************************************
+*  Hardware
+*******************************************************
+
+OS: Window 10
+Processor: Intel(R) Core(TM) i7-6500U 
+Graphic Card: Intel(R) HD Graphic 520
+RAM: 8 GB
---
+++ Robot Arm/Kulprawee_Prayoonsuk_HW2.cpp
@@ -0,0 +1,241 @@
+Keyboard commands:
+(Clockwise / Counterclock wise)
+s/S - shoulder
+e/E - elbow
+h/H - hand(wrist)
+1/! - finger 1
+2/@ - finger 2
+3/# - finger 3
+*/
+
+//Necessary Libraries:
+#include <GL/glut.h>
+
+static int shoulder = 0, elbow = 0, hand = 0, f1 = 0, f2 = 0, f3 = 0;
+
+//Elbow
+void elbowAdd(void)
+{
+	elbow = (elbow + 5) % 360;
+}
+
+void elbowSubtract(void)
+{
+	elbow = (elbow - 5) % 360;
+}
+
+//Shoulder
+void shoulderAdd(void)
+{
+	shoulder = (shoulder + 5) % 360;
+}
+
+void shoulderSubtract(void)
+{
+	shoulder = (shoulder - 5) % 360;
+}
+
+//Hand
+void handAdd(void)
+{
+	hand = (hand + 5) % 360;
+}
+
+void handSubtract(void)
+{
+	hand = (hand - 5) % 360;
+}
+
+//Finger 1
+void f1Add(void)
+{
+	f1 = (f1 + 5) % 360;
+}
+
+void f1Subtract(void)
+{
+	f1 = (f1 - 5) % 360;
+}
+
+//Finger 2
+void f2Add(void)
+{
+	f2 = (f2 + 5) % 360;
+}
+
+void f2Subtract(void)
+{
+	f2 = (f2 - 5) % 360;
+}
+
+//Finger 3
+void f3Add(void)
+{
+	f3 = (f3 + 5) % 360;
+}
+
+void f3Subtract(void)
+{
+	f3 = (f3 - 5) % 360;
+}
+//-------------------------------------------------------------------
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 1.0);
+
+	glPushMatrix();
+
+	/* shoulder */
+	glTranslatef(-1.0, 0.0, 0.0);	//the origin of where it will rotate about
+	glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
+	glTranslatef(0.5, 0.0, 0.0);	//the placement will be first translate + second 
+	//push new matrix - that why it only scale this current matrix
+	glPushMatrix();
+	glScaled(0.5, 0.3, 0.5);
+	glutWireCube(2.0);
+	glPopMatrix();
+
+	/* elbow */
+	glTranslatef(0.5, 0.0, 0.0);
+	glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
+	glTranslatef(0.5, 0.0, 0.0);
+	glPushMatrix();
+	glScaled(0.5, 0.3, 0.5);
+	glTranslatef(-.0, 0.0, 0.0);
+	glutWireCube(2.0);
+	glPopMatrix();
+
+	/* hand*/
+	glTranslatef(0.5, 0.0, 0.0);
+	glRotatef((GLfloat)hand, 0.0, 0.0, 1.0);
+	glTranslatef(0.5, 0.0, 0.0);
+	glPushMatrix();
+	glScaled(0.5, 0.3, 0.5);
+	glutWireCube(2.0);
+	glPopMatrix();
+
+	glPushMatrix();		//the finger need extra push and pop to make it start in the hand matrix
+	/* f1*/
+	glTranslatef(0.5, 0.0, 0.0);
+	glRotatef((GLfloat)f1, 0.0, 0.0, 1.0);
+	glTranslatef(0.18, 0.0, 0.0);
+	glPushMatrix();
+	glScaled(0.3, 0.2, 0.3);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPushMatrix();
+	/* f2*/
+
+	glTranslatef(0.5, 0.0, 0.0);
+	glRotatef((GLfloat)f2, 0.0, 0.0, 1.0);
+	glTranslatef(0.18, 0.0, 0.3);
+	glPushMatrix();
+	glScaled(0.3, 0.2, 0.3);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPushMatrix();
+	/* f3*/
+
+	glTranslatef(0.5, 0.1, 0.0);							
+	glRotatef((GLfloat)f3, 0.0, 0.0, 1.0);
+	glTranslatef(0.18, 0.1, 0.0);
+	glPushMatrix();
+	glScaled(0.3, 0.2, 0.3);
+	glutWireCube(1.0);
+	glPopMatrix();
+
+	glPopMatrix();
+
+	glPopMatrix();
+	glFlush();
+}
+
+//-------------------------------------------------------------------
+
+void myinit(void)
+{
+	glShadeModel(GL_FLAT);
+}
+
+//-------------------------------------------------------------------
+void myReshape(GLsizei w, GLsizei h)
+{
+	glViewport(0, 0, w, h);	//set view port 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(65.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glTranslatef(0.0, 0.0, -5.0);
+}
+
+//-------------------------------------------------------------------
+//keyboard input
+void mykeyboard(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'S':
+		shoulderAdd();
+		break;
+	case 's':
+		shoulderSubtract();
+		break;
+	case 'E':
+		elbowAdd();
+		break;
+	case 'e':
+		elbowSubtract();
+		break;
+	case 'H':
+		handAdd();
+		break;
+	case 'h':
+		handSubtract();
+		break;
+	case '1':
+		f1Add();
+		break;
+	case '!':
+		f1Subtract();
+		break;
+	case '2':
+		f2Add();
+		break;
+	case '@':
+		f2Subtract();
+		break;
+	case '3':
+		f3Add();
+		break;
+	case '#':
+		f3Subtract();
+		break;
+	default:
+		break;
+	}
+	glutPostRedisplay();		//redisplay the screen after each key press
+}
+
+//-------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1000, 1000);
+	glutCreateWindow("Kulprawee Prayoonsuk - HW2 - Robot Hand");
+	myinit();
+	glutDisplayFunc(display);
+	glutReshapeFunc(myReshape);
+	glutKeyboardFunc(mykeyboard);
+	glutMainLoop();
+}
